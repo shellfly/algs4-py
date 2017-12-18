998913ad837488570fb61a4f389497f093415af7
shellfly <shell0fly@gmail.com>
2017-12-18 16:23:41 +0800

    add setup files

---
+++ README.rst
@@ -0,0 +1,36 @@
+## Overview
+
+This repository contains the Python source code for the algorithms in the textbook
+<a href = "http://amzn.to/13VNJi7">Algorithms, 4th Edition</a> by Robert Sedgewick and Kevin Wayne.
+This is NOT the official version.
+The programs are organized in the package <code>algs4</code>.
+
+
+## Goals
+
+Make a Python implementation of the library so that a Python programmer can learn this book easily.
+
+Try to keep the interface and variable name consistent with the original book without violating the Python coding standards.
+
+
+
+## Installing 
+
+`pip install algs4`
+
+
+## Usage
+
+`from algs4 import *`
+or
+`from algs4 import Stack`
+
+
+## License
+
+This code is released under MIT.
+
+## Contribute to this repository
+
+Issue reports and code fixes are welcome. please follow the same style as the code in the repository and thoroughly test your
+code.
---
+++ setup.py
@@ -0,0 +1,35 @@
+"""A Python library for book algs4
+
+See:
+https://algs4.cs.princeton.edu/home/
+https://github.com/shellfly/algs4-py
+"""
+
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='algs4',
+    version='1.0.0',
+    description='A Python implementation library for book algs4',
+    long_description=long_description,
+    url='https://github.com/shellfly/algs4-py',
+    author='shellfly',
+    author_email='shell0fly@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+
+    keywords='algs4 algorithm',  # Optional
+    packages=['algs4']
+)
