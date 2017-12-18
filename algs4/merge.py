26e7d9e4b8680ef6684b7e885d4487ee879c07ef
shellfly <shell0fly@gmail.com>
2017-12-18 23:05:45 +0800

    add quick sort

--- algs4/merge.py
+++ algs4/merge.py
@@ -26,25 +26,40 @@ all bad bed bug dad ... yes yet zoo    [ one string per line ]
 class Merge:
 
     @classmethod
-    def merge(cls, arr):
+    def merge(cls, arr, lo, mid, hi):
+        aux = list(arr)  # copy to aux
+        i = lo
+        j = mid + 1
+        k = lo
+        while k <= hi:
+            if i > mid:
+                arr[k] = aux[j]
+                j += 1
+            elif j > hi:
+                arr[k] = aux[i]
+                i += 1
+            elif aux[i] < aux[j]:
+                arr[k] = aux[i]
+                i += 1
+            else:
+                arr[k] = aux[j]
+                j += 1
+            k += 1
+
+    @classmethod
+    def mergesort(cls, arr, lo, hi):
+        if lo >= hi:
+            return
+
+        mid = (lo + hi) / 2
+        cls.mergesort(arr, lo, mid)
+        cls.mergesort(arr, mid + 1, hi)
+        cls.merge(arr, lo, mid, hi)
+        return arr
 
     @classmethod
     def sort(cls, arr):
-        N = len(arr)
-        h = 1
-        while h < N / 3:
-            h = 3 * h + 1  # 1, 4, 13, 40...
-
-        while h >= 1:
-            for i in range(h, N):
-                j = i
-                while j >= h:
-                    if arr[j] > arr[j - h]:
-                        break
-                    arr[j], arr[j - h] = arr[j - h], arr[j]
-                    j -= h
-            h /= 3
-        return arr
+        return cls.mergesort(arr, 0, len(arr) - 1)
 
 if __name__ == '__main__':
     import sys
---
+++ algs4/quick.py
@@ -0,0 +1,64 @@
+"""
+Sorts a sequence of strings from standard input using quick sort.
+
+ 
+% more tiny.txt
+
+S O R T E X A M P L E
+
+% python quick < tiny.txt
+
+A E E L M O P R S T X                 [ one string per line ]
+
+  
+
+% more words3.txt
+
+bed bug dad yes zoo ... all bad yet
+
+
+% python quick < words3.txt
+
+all bad bed bug dad ... yes yet zoo    [ one string per line ]
+"""
+
+
+class Quick:
+
+    @classmethod
+    def partition(cls, arr, lo, hi):
+        v = arr[lo]
+        i = lo + 1
+        j = hi
+        while True:
+            while i < hi and arr[i] < v:
+                i += 1
+            while j > lo and arr[j] > v:
+                j -= 1
+            if i >= j or i == hi or j == lo:
+                break
+            arr[i], arr[j] = arr[j], arr[i]
+        arr[lo], arr[j] = arr[j], arr[lo]
+        return j
+
+    @classmethod
+    def quicksort(cls, arr, lo, hi):
+        if lo >= hi:
+            return
+
+        j = cls.partition(arr, lo, hi)
+        cls.quicksort(arr, lo, j - 1)
+        cls.quicksort(arr, j + 1, hi)
+        return arr
+
+    @classmethod
+    def sort(cls, arr):
+        return cls.quicksort(arr, 0, len(arr) - 1)
+
+if __name__ == '__main__':
+    import sys
+
+    for line in sys.stdin:
+        items = line.split()
+        print('     items: ', items)
+        print('sort items: ', Quick.sort(items))
