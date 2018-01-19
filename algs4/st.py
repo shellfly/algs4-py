a91d5b2ccd27405d4df641243c0e59650247c426
shellfly <shell0fly@gmail.com>
2018-01-19 16:25:26 +0800

    add frequence_counter && sequential_search_st

---
+++ algs4/st.py
@@ -0,0 +1,30 @@
+"""
+  Execution:    python st < input.txt
+  Data files:   https://algs4.cs.princeton.edu/35applications/tinyST.txt
+  
+  Does not allow duplicates.
+ """
+
+
+class ST:
+
+    def put(self, key, value):
+        pass
+
+    def get(self, key):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def contains(self, key):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def size(self):
+        pass
+
+    def keys(self):
+        pass
